8774fb059d11980c92b42a6da2a4fd055128cac0
Simon Tamas Jozsef <simontjozsef@gmail.com>
2019-05-12 13:55:58 +0200

    initial commit

---
+++ DataModel/Choice.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace KJK.DataModel {
+	public class Choice{
+		public int ID { get; set; }
+        public string Text { get; set; }
+        public int Reference { get; set; }
+        public bool Special { get; set; }
+        public Paragraph Paragraph { get; set; }
+    }
+}
---
+++ DataModel/Monster.cs
@@ -0,0 +1,13 @@
+﻿namespace KJK.DataModel {
+
+    public class Monster {
+		
+		public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Health { get; set; }
+        public int Agility { get; set; }
+        public Paragraph Paragraph { get; set; }
+
+    }
+}
