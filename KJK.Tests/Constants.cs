e4dc22cc143cd575168502be2d50b5ae71d76460
Tamás Simon <tsimon@geomant.com>
2020-04-09 17:02:12 +0200

    created constants class for Test project

---
+++ KJK.Tests/Constants.cs
@@ -0,0 +1,28 @@
+﻿using KJK.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace KJK.Tests
+{
+	public static class Constants
+	{
+		public static DbContextOptions<KJKDbContext> GetOptions(DbType type) {
+			var builder =  new DbContextOptionsBuilder<KJKDbContext>();
+			
+			switch (type)
+			{
+				case DbType.InMemory:
+					builder.UseInMemoryDatabase("KJKDB");
+					break;
+				case DbType.SqlServer:
+					builder.UseSqlServer("Server = localhost; Database = KJKDB; Trusted_Connection = True;");
+					break;
+				default:
+					return null;
+			}
+
+			return builder.Options;
+
+		}
+	}
+	public enum DbType { InMemory,SqlServer}
+}
