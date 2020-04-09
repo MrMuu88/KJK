be4ae5a08462295e846ba5be8d60a20ba78618d6
Tamás Simon <tsimon@geomant.com>
2020-04-09 17:58:50 +0200

    created init migration

---
+++ KJK.Data/Migrations/20200409154543_init.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KJK.Data.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Paragraphs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Text = table.Column<string>(nullable: true),
+                    Choices = table.Column<string>(nullable: true),
+                    SpecialEvent = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Paragraphs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Weight = table.Column<float>(nullable: false),
+                    MaxStack = table.Column<int>(nullable: false),
+                    IsConsumable = table.Column<bool>(nullable: false),
+                    Slot = table.Column<int>(nullable: false),
+                    Type = table.Column<int>(nullable: false),
+                    ParagraphId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Items_Paragraphs_ParagraphId",
+                        column: x => x.ParagraphId,
+                        principalTable: "Paragraphs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Monsters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Strength = table.Column<int>(nullable: false),
+                    Stamina = table.Column<int>(nullable: false),
+                    Dexterity = table.Column<int>(nullable: false),
+                    Intelect = table.Column<int>(nullable: false),
+                    Wisdom = table.Column<int>(nullable: false),
+                    Charisma = table.Column<int>(nullable: false),
+                    Health = table.Column<int>(nullable: false),
+                    Mana = table.Column<int>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    ParagraphId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Monsters", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Monsters_Paragraphs_ParagraphId",
+                        column: x => x.ParagraphId,
+                        principalTable: "Paragraphs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_ParagraphId",
+                table: "Items",
+                column: "ParagraphId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Monsters_ParagraphId",
+                table: "Monsters",
+                column: "ParagraphId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Items");
+
+            migrationBuilder.DropTable(
+                name: "Monsters");
+
+            migrationBuilder.DropTable(
+                name: "Paragraphs");
+        }
+    }
+}
